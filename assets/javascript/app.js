e45253121509ff7cacafa12819b6f83e72316024
Jonathan Pham <jonathanthienpham@gmail.com>
2020-02-29 09:25:24 -0500

    mobile edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,12 @@
 //To store Questions, answers, and memes into array for storage
 //Time needs to be set promptly ex: Have a on click function in order for the timer to start
 //How do we set a timer to show and preview each question? By having a function in javascript (ex: 30seconds & timeUP)
+
 //If they don't press any answer or let the timer run down to 0 then we increase "Unanswered Question by 1" - If they click the right answer the correct answer increases by 1
 //Regardless, the solution meme will pop up (Each question has the same process of 30 seconds) Have the solution shown for atleast 5 seconds
 //How do we show the solution if the user answers the question correctly or wrong? By hide & show using jquery
 //How do I show the scoreboard once the user finishes all the questions promptly? Correct, Incorrect, and Unanswered
+
 //-- How do I keep track of the answers above and have it apend?
 //Once the scoreboard is shown; have a question if the user wants to restart
 
